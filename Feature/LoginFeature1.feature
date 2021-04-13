981328b1daed68e8b75596a8d9fe86c5d3ce6ab3
minadangal1 <amritsa2012@gmail.com>
2021-04-13 18:47:36 -0500

    Initial commit

---
+++ Feature/LoginFeature1.feature
@@ -0,0 +1,15 @@
+﻿Feature: LoginFeature1
+	
+
+@mytag
+@mytag
+Scenario: I try to Login account using Valid and invalid UserName and Password
+	Given I am Navigate to the Url "http://demo.guru99.com/V1/index.php";
+	And I enter the Login information
+	| UserId     | Password | Status  |
+	| mngr318486 | UrYgYpA  | Valid   |
+	| asgdkdga   | sjvaam   | Invalid |
+	|            | UrYgYpA  | Invalid |
+	| mngr318486 | UrYgYpA  | Valid   |
+    Then the click the login button
+
---
+++ StepeDefinetion/StepDefinition1.cs
@@ -0,0 +1,76 @@
+﻿using NUnitTestProject17.Helper;
+using NUnitTestProject17.Test;
+using NUnitTestProject17.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace NUnitTestProject1.StepeDefinetion
+{
+    [Binding]
+    public sealed class StepDefinition1:BasepageTest
+    {
+        public StepDefinition1() : base()
+        {
+
+        }
+        [Given(@"I am Navigate to the Url ""(.*)"";")]
+        public void GivenIAmNavigateToTheUrl(string p0)
+        {
+            FrameworkHelper.WebDriver.Url = p0;
+        }
+
+        [Given(@"I enter the Login information")]
+        public void GivenIEnterTheLoginInformation(Table table)
+        {
+            var tebleData = table.CreateDynamicSet();
+            foreach (var item in tebleData)
+            {
+                //  for (int i = 0; i < table.RowCount; i++)
+                //  {
+
+                SigninPageView signinPageView = new SigninPageView();
+
+                signinPageView.SetuserName(item.UserId);
+                signinPageView.SetPassword(item.Password);
+                signinPageView.ClickLoginButton();
+
+                
+
+                if (FrameworkHelper.IfAlertIsPresent() == true)
+                {
+                    FrameworkHelper.WebDriver.SwitchTo().Alert().Accept();
+                    FrameworkHelper.WebDriver.SwitchTo().DefaultContent();
+                }
+               
+                else
+                {
+
+                    
+                    signinPageView.ClickLogout();
+                    FrameworkHelper.WebDriver.SwitchTo().Alert().Accept();
+                    FrameworkHelper.WebDriver.SwitchTo().DefaultContent();
+
+                }
+               
+
+
+
+            }
+        }
+
+        [Then(@"the click the login button")]
+        public void ThenTheClickTheLoginButton()
+        {
+            SigninPageView signinPageView = new SigninPageView();
+            signinPageView.SetuserName("mngr318486");
+            signinPageView.SetPassword("UrYgYpA");
+            signinPageView.ClickLoginButton();
+        }
+
+
+    }
+}
